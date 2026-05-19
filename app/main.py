beb240209cd32992e66ef0e8f167944b25f548a5
Hemues <hemues@hemues.com>
2026-05-19 23:29:27 +0200

    fix: remove 'use client driver = yes' so Samba serves Point&Print drivers
    Without this, Samba tells Windows to use its own local driver instead of
    downloading from print$. Removing it enables silent driver install on
    double-click from \\11.1.0.4.

--- app/main.py
+++ app/main.py
@@ -853,7 +853,6 @@ def _smb_printer_block(name: str, comment: str = '') -> str:
         f'   guest ok = no\n'
         f'   read only = yes\n'
         f'   create mask = 0700\n'
-        f'   use client driver = yes\n'
         f'   force user = root\n'
     )
 
